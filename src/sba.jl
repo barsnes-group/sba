423411b4c1b18ef11e789655ca43bcae03bf4b8b
bramburger <find.bram.burger@gmail.com>
2020-11-18 15:37:44 +0100

    Added missing function updating lambda

--- src/sba.jl
+++ src/sba.jl
@@ -6,12 +6,12 @@ function getStartPair(lambda, groups)
     minimum=lambda[groups[1],groups[2]]
     minarr=[groups[1] groups[2]]
     for i=1:(length(groups)-1), j=(i+1):length(groups)
-	if lambda[groups[i],groups[j]] == minimum
-	    minarr = vcat(minarr, [groups[i] groups[j]])
-	elseif lambda[groups[i],groups[j]] < minimum
-	    minimum=lambda[groups[i],groups[j]]
-	    minarr=[groups[i] groups[j]]
-	end
+    if lambda[groups[i],groups[j]] == minimum
+        minarr = vcat(minarr, [groups[i] groups[j]])
+    elseif lambda[groups[i],groups[j]] < minimum
+        minimum=lambda[groups[i],groups[j]]
+        minarr=[groups[i] groups[j]]
+    end
     end
     minarr[sample(1:size(minarr, 1), 1)[1], :]
 end
@@ -19,6 +19,17 @@ end
 function getStartSingle(lambda, groups)
     groups[sample(findall(lambda[diagind(lambda)[groups]] .== minimum(lambda[diagind(lambda)[groups]])), 1)[1]]
 end
+
+function fixLambda(allocations::Array{Int})
+    lambda = zeros(Int, (size(allocations)[2], size(allocations)[2]))
+    lambda[diagind(lambda)] .= dropdims(sum(allocations, dims=1), dims=1)
+    for i=1:(size(lambda)[1]-1), j=i:size(lambda)[1]
+        lambda[i,j] = min(lambda[i,i], lambda[j,j])
+        lambda[j,i] = min(lambda[i,i], lambda[j,j])
+    end
+    lambda
+end
+
 
 function sba(samplesizes, batchsizes)
     sba(copy(samplesizes), copy(batchsizes))
@@ -43,26 +54,26 @@ function sba!(samplesizes, batchsizes)
                     replace=false, ordered=true))
             else
                 chosen = vcat(chosen, neverChosen)
-		while length(chosen) < batchsizes[bsi]
-		    colsums = dropdims(sum(view(lambda, chosen,
+        while length(chosen) < batchsizes[bsi]
+            colsums = dropdims(sum(view(lambda, chosen,
                                                 filter(x -> x ∉ chosen, groups)), dims=1), dims=1)
-		    nextchoice = sample(findall(colsums .== minimum(colsums)), 1)[1]
-		    chosen = vcat(chosen, filter(x -> x ∉ chosen, groups)[nextchoice])
-		end
+            nextchoice = sample(findall(colsums .== minimum(colsums)), 1)[1]
+            chosen = vcat(chosen, filter(x -> x ∉ chosen, groups)[nextchoice])
+        end
             end
         elseif length(chosen) < batchsizes[bsi] # if length(neverChosen)
             if length(chosen) == 0 # pick first pair of treatments
-		if batchsizes[bsi] == 1
-		    chosen = vcat(chosen, getStartSingle(lambda, groups))
-		else
-		    chosen = getStartPair(lambda, groups)
-		end
-	    end
-	    while length(chosen) < batchsizes[bsi]
-		colsums = dropdims(sum(view(lambda, chosen, filter(x -> x ∉ chosen, groups)), dims=1), dims=1)
-		nextchoice = sample(findall(colsums .== minimum(colsums)), 1)[1]
-		chosen = vcat(chosen, filter(x -> x ∉ chosen, groups)[nextchoice])
-	    end
+        if batchsizes[bsi] == 1
+            chosen = vcat(chosen, getStartSingle(lambda, groups))
+        else
+            chosen = getStartPair(lambda, groups)
+        end
+        end
+        while length(chosen) < batchsizes[bsi]
+        colsums = dropdims(sum(view(lambda, chosen, filter(x -> x ∉ chosen, groups)), dims=1), dims=1)
+        nextchoice = sample(findall(colsums .== minimum(colsums)), 1)[1]
+        chosen = vcat(chosen, filter(x -> x ∉ chosen, groups)[nextchoice])
+        end
         end # if length(neverChosen) else
         allocations[bsi, chosen] .+= 1
         for rowi=chosen, coli=chosen
